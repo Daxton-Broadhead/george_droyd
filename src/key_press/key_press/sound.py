aff82f76f1de062245956a59226c4f2311be565b
Daxton Broadhead <daxton.broadhead21@gmail.com>
2025-07-21 23:04:08 -0400

    checkpoint

--- src/key_press/key_press/sound.py
+++ src/key_press/key_press/sound.py
@@ -1,10 +1,17 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String  # Adjust if using a different type
+from std_msgs.msg import Int32
 import os
-from playsound import playsound  # Optional: your "do something"
+
 import sys
-print(f"Python: {sys.executable}")
+import simpleaudio as sa
+
+print("Current working directory:", os.getcwd())
+
+
+
+
 
 class KeyPressListener(Node):
     
@@ -12,7 +19,7 @@ class KeyPressListener(Node):
     def __init__(self):
         super().__init__('key_press_listener')
         self.subscription = self.create_subscription(
-            String,                   # Message type
+            Int32,                   # Message type
             '/keyboard/keypress',         # Topic name
             self.listener_callback,   # Callback function
             10                        # QoS
@@ -20,17 +27,22 @@ class KeyPressListener(Node):
         print(f"Python: {sys.executable}")
 
     def listener_callback(self, msg):
-        key = msg.data.strip().lower()  # Normalize input
+        key = msg.data  # Normalize input
+        print(key)
         self.get_logger().info(f"Received key press: {key}")
-        if key == 'n':
+        if key == 78:
             self.handle_n_press()
 
     def handle_n_press(self):
+        print('stuff is being done')
         script_path = os.path.abspath(__file__)
         self.get_logger().info("Key 'n' was pressed! Doing something...")
         # Do your action here (e.g., play sound)
-        # sound_path = '/absolute/path/to/beep.wav'
-        #playsound(script_path + '/../sounds/yeetSound.mp3')
+        wave_obj = sa.WaveObject.from_wave_file("src/key_press/sounds/yeetSound.wav")
+        wave_obj.play()
+        # play_obj = wave_obj.play()
+        # play_obj.wait_done()  # Wait until sound has finished playing
+
 
 def main(args=None):
     rclpy.init(args=args)
